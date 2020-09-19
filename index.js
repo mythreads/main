348743c331a15c1ec866b7fbdfb86fd8b2f2173d
hahnbeelee <hl985@cornell.edu>
2020-09-19 11:26:29 -0400

    cow should show up now

--- index.js
+++ index.js
@@ -2,16 +2,13 @@ const express = require("express");
 const cowsay = require("cowsay");
 const cors = require("cors");
 const path = require("path");
+
+// Create the server
 const app = express();
 
 // Serve static files from the React frontend app
 app.use(express.static(path.join(__dirname, "client/build")));
-// Anything that doesn't match the above, send back index.html
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
-});
 
-// Create the server
 // Serve our api route /cow that returns a custom talking text cow
 app.get("/api/cow/:say", cors(), async (req, res, next) => {
   try {
@@ -22,7 +19,8 @@ app.get("/api/cow/:say", cors(), async (req, res, next) => {
     next(err);
   }
 });
-// Serve our base route that returns a Hello World cow
+
+// Serve our base route that returns a Hellow World cow
 app.get("/api/cow/", cors(), async (req, res, next) => {
   try {
     const moo = cowsay.say({ text: "Hello World!" });
@@ -31,6 +29,12 @@ app.get("/api/cow/", cors(), async (req, res, next) => {
     next(err);
   }
 });
+
+// Anything that doesn't match the above, send back the index.html file
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
 // Choose the port and start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {

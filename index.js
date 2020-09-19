2d06d845b2464fea4922c6ac53267d08ec3caa58
hahnbeelee <hl985@cornell.edu>
2020-09-19 11:10:19 -0400

    Deploy takeoff!

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const cowsay = require("cowsay");
 const cors = require("cors");
+const path = require("path");
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, "client/build")));
+// Anything that doesn't match the above, send back index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
 // Create the server
 const app = express();
 // Serve our api route /cow that returns a custom talking text cow
